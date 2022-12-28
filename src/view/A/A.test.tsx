f6ea5d8f1b965321d4786795f5704cf8dcd5e39e
c332030 <c332030@gmail.com>
2022-12-28 10:47:49 +0800

    feat: A test

--- src/view/A/A.test.tsx
+++ src/view/A/A.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import A from './A';
 
-test('renders learn react link', () => {
+test('renders A link', () => {
   render(<A />);
   const linkElement = screen.getByText(/下载/i);
   expect(linkElement).toBeInTheDocument();

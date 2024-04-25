d7be807067ee0e1313b12d29afaf990d9d414f55
c332030 <c332030@c332030.com>
2024-04-25 16:47:31 +0800

    feat: IDUtils

---
+++ src/utils/IDUtils.js
@@ -0,0 +1,12 @@
+/**
+ * <p>
+ *   Description: IDUtils
+ * </p>
+ * @author c332030
+ * @since 2024/4/25
+ */
+import {nanoid} from "nanoid";
+
+export function getNanoid() {
+    return nanoid();
+}

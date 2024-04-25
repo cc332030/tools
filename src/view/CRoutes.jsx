4f1d599ee980364a393b994f94ed0c44cb83036f
c332030 <c332030@c332030.com>
2024-04-25 16:01:32 +0800

    feat: routes

--- src/view/CRoutes.jsx
+++ src/view/CRoutes.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import ExtractUrl from "view/ExtractUrl";
 import Home from "view/Home";
 import NotFound from "view/NotFound";
+import Proxy from "view/Proxy";
 
 /**
  * <p>
@@ -10,13 +11,23 @@ import NotFound from "view/NotFound";
  * @author c332030
  * @since 2024/4/24
  */
+
+export const allRoutes = {
+    "/": <Home />,
+    "/proxy": <Proxy />,
+    "/extract-url": <ExtractUrl />,
+    "*": <NotFound />,
+}
+
 export default function CRoutes() {
     return (
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/extract-url" element={<ExtractUrl />}/>
-            <Route path="*" element={<NotFound />}/>
+              {
+                  Object.entries(allRoutes).map(([path, view]) => (
+                      <Route path={path} element={view}/>
+                  ))
+              }
           </Routes>
         </BrowserRouter>
     );

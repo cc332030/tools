18ae70101877c61e618ca04a15f9a137b36f66bb
c332030 <c332030@c332030.com>
2024-04-26 16:31:24 +0800

    feat: UrlUtils

--- src/utils/UrlUtils.js
+++ src/utils/UrlUtils.js
@@ -6,6 +6,22 @@
  * @since 2024/4/26
  */
 
+export class UrlUtils {
+
+}
+
+export function getDomain(url) {
+    if(!url) {
+        return null;
+    }
+
+    return url.split('/').slice(0, 3).join('/')
+}
+
+export function checkUrl(url) {
+    return url && url.startsWith('http');
+}
+
 /**
  *
  * @param text {string}
